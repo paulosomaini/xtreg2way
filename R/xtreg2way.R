02be685cd02fa4df3a865a983f1e9be59414acf7
Brian Chivers <bchivers@yen3.stanford.edu>
2020-05-15 08:42:33 -0700

    formula work etc

--- R/xtreg2way.R
+++ R/xtreg2way.R
@@ -18,8 +18,8 @@
 #'        se=="2" : standard errors robust to heteroscedasticity but assumes no correlation within group or serial correlation.
 #'        se=="11" : Arellano standard errors with a degree of freedom correction performed by Stata xtreg, fe.
 #'        If se is omitted or set to [] then it is set to 1 and the Arellano (1987) estimator is computed.
-#'
 #' @param noise (optional) If noise is set to "1", then results are displayed
+#' @param data (optional) If the first argument is a formula, you can pass a dataframe here
 #' @return betaHat (K-by-1) vector of estimated coefficients
 #' aVarHat (K-by-K) estimate of the matrix of variances and covariance of  the estimator.
 #' yp (N-by-1) the residual of the projection of y on the two sets of  dummies.
@@ -33,15 +33,59 @@
 #' tid <- c("1","1" ,"1" ,"2","2" ,"3","3","3" ,"4","4","5" ,"5","6","6" ,"6")
 #' w <- rep(1, 15)
 #' x1 <- rnorm(15, mean=50, sd=10)   
+#' x2 <- rnorm(15, mean=50, sd=10)   
 #' y <- x1 + rnorm(15, mean=50, sd=10)    
+#' #The most basic way to use this function
 #' output <- xtreg2way(y, x1, hhid, tid, w, se="2", noise="1")
+#' #You can rerun faster with different columns using output$struc
+#' output2 <- xtreg2way(y, data.frame(x1,x2), struc=output$struc)
+#' #Or you can use a formula and specify data=
+#' output3 <- xtreg2way(y~x1+x2, iid=hhid, tid=tid, w=w, se="2", noise="1",
+#'                     data=data.frame(x1,x2,y))
 #' @export
 
 
-xtreg2way <- function(y, X, iid = NULL, tid = NULL, w = NULL, struc = NULL,
-                      se = "", noise = NULL) {
+xtreg2way <- function(y, X=NULL, iid = NULL, tid = NULL, w = NULL, 
+                      struc = NULL, se = "", noise = NULL, data = NULL) {
+  #This variable is needed for the return at the bottom
   struc_is_null <- is.null(struc)
+  #If struc is passed, grab iid tid and w from it
+  if (!is.null(struc)) {
+    iid <- struc$hhid
+    tid <- struc$tid
+    w <- struc$w
+  }
+  #If a formula is passed, grab all necessary variables
+  if( class(y) == "formula") {
+    
+    #These variables should be passed explicitly or via struc by now
+    if (is.null(iid) | is.null(tid)) {
+      stop(paste("Arguments Missing:",
+                 "If the first argument is a formula,",
+                 "then iid and tid are required arguments"), sep = " ")
+    }
+    
+    #Check to see if labels exist
+    for (label in attr(stats::terms(y),"term.labels")) {
+      if (!label %in% labels(data)[[2]]){
+        stop(paste("Missing Variable:",
+                   label,"is not in data", sep=" "))
+      }
+    }
+    #Build X
+    X <- data[attr(stats::terms(y),"term.labels")]
+    
+    #Make sure only 1 y variable exists
+    y_label <- setdiff(all.vars(y) , attr(stats::terms(y),"term.labels"))
+    if( length(y_label) > 1) {
+      stop(paste("Multiple independent variables in formula",
+                 y_label, sep=" "))
+    }
+    y <- data[y_label]
+  }
+  
   X <- as.matrix(X)
+  y <- as.matrix(y)
   obs <- dim(X)[1]
   K <- dim(X)[2]
 
@@ -52,25 +96,23 @@ xtreg2way <- function(y, X, iid = NULL, tid = NULL, w = NULL, struc = NULL,
 
   #checking y and X dimensions
   if (length(y) != obs) {
-    print(paste("Error, Dimension Mismatch:",
+    stop(paste("Error, Dimension Mismatch:",
           "y must be a vector of length N,",
           "and X must be a N by K matrix"), sep = " ")
   }
 
   #If struc isn't provided
-  if (struc_is_null) {
+  if (is.null(struc)) {
     #we need iid and tid
     if (is.null(tid) | is.null(iid)) {
-      print(paste("Error: if struc isn't provided,",
+      stop(paste("Error: if struc isn't provided,",
             "iid and tid are required arguments"))
-      return()
     }
     #Check length of iid and tid
     if (length(iid) != obs | length(tid) != obs) {
-      print(paste("Error, Dimension Mismatch:",
+      stop(paste("Error, Dimension Mismatch:",
             "iid and tid must be vectors of length N,",
             "matching the length of y and rows of X"), sep = " ")
-      return()
     }
     #Build struc
     struc <- projdummies(iid, tid, w)
@@ -84,7 +126,7 @@ xtreg2way <- function(y, X, iid = NULL, tid = NULL, w = NULL, struc = NULL,
   } else {
     #Else, if struc is provided, check length of hhid and tid
     if (length(struc$hhid) != obs | length(struc$tid) != obs) {
-      print(paste("Error, Dimension Mismatch:",
+      stop(paste("Error, Dimension Mismatch:",
             "struc$hhid and struc$tid must be vectors of length N,",
             "matching the length of y and rows of X"), sep = " ")
     }
@@ -127,6 +169,7 @@ xtreg2way <- function(y, X, iid = NULL, tid = NULL, w = NULL, struc = NULL,
     aVarHat <- avar(X, reg$res, struc$hhid, reg$XX)
   }
 
+  #Build the noise DF and print it
   if (!is.null(noise)) {
     std <- sqrt(diag(aVarHat))
     df <- data.frame(coefficients = as.numeric(betaHat), se = std,
@@ -135,6 +178,7 @@ xtreg2way <- function(y, X, iid = NULL, tid = NULL, w = NULL, struc = NULL,
     print(df)
   }
 
+  #Return all that is needed
   return_list <- list()
   return_list$betaHat <- betaHat
   return_list$aVarHat <- aVarHat
